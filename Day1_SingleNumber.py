da11227592e462130f13604697fc846a691bfedf
shravya-k <k.shravya96@gmail.com>
2020-04-21 19:16:37 +0530

    deleting day1

--- Day1_SingleNumber.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        temp=0
-        for i in range(len(nums)):
-            temp^=nums[i]
-        return(temp)
-            
-            
